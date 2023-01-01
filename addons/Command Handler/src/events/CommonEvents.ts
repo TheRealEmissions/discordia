d76c09770fe4fffc55dde77b5dc49963cf04f060
therealemissions <me@liammartin.site>
2023-01-01 18:44:20 +0000

    updated

--- addons/Command Handler/src/events/CommonEvents.ts
+++
@@ -1,7 +0,0 @@
-export enum CommandHandlerEvents {
-  APPLICATION_COMMAND_USE = "applicationCommandUse",
-  BUTTON_CLICK = "buttonClick",
-  CONTEXT_MENU_USE = "contextMenuUse",
-  SELECT_MENU_CLICK = "selectMenuClick",
-  MODAL_SUBMIT = "modalSubmit",
-}
--- addons/Events/src/enums/EventTypes.ts
+++
@@ -1,4 +0,0 @@
-export enum EventTypes {
-  ON = "on",
-  ONCE = "once",
-}
